cadcd7866eb084e3f9f30ce176879354743d6e24
Otavio Henrique <otavio.henrique@hotmail.com>
2017-09-12 22:01:32 -0300

    Versionist and API versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
   namespace :api, defaults: { format: :json  }, constraints: { subdomain: "api" }, path: "/" do
-    
+    api_version(:module => "V1", :path => {:value => "v1"}) do
+      resources :tasks
+    end
   end
 end
