d43b183fa120bee1aa27513bb1720c95939e5448
Otavio Henrique <otavio.henrique@hotmail.com>
2017-09-15 21:18:11 -0300

    Adding devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  devise_for :users
   namespace :api, defaults: { format: :json  }, constraints: { subdomain: "api" }, path: "/" do
     api_version(:module => "V1", :path => {:value => "v1"}) do
       resources :tasks
