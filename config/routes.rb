48e0b8cd53229f72aa5efdb479fc241400a4c736
Otavio Henrique <otavio.henrique@hotmail.com>
2017-09-16 22:34:22 -0300

    Adding users route (index)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json  }, constraints: { subdomain: "api" }, path: "/" do
     api_version(:module => "V1", :path => {:value => "v1"}) do
-      resources :tasks
+      resources :users, only: [:show]
     end
   end
 end
---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe 'Users API', type: :request  do
+  let!(:user) { create(:user) } 
+  let(:user_id) { user.id }
+  
+  before { host! "api.task-manager.dev:80/v1" }
+
+  describe "GET /users/:id" do
+    before do
+      headers = { "Accept" => "application/json" }
+      get "/users/#{user_id}", params: {}, headers: headers
+    end
+
+    context "when user exists" do
+      it "should return the user" do
+        user_response = JSON.parse(response.body)
+        expect(user_response["id"]).to eq(user_id)
+      end
+
+      it "should return 200 status" do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "when users dont exist" do
+      let(:user_id) { 40 }
+
+      it "should return 404 status" do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+end
