4adf51410b71ef8aadc4558f5f319fdb5597f85c
Otavio Henrique <otavio.henrique@hotmail.com>
2017-09-24 19:24:04 -0300

    Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
  
-  # devise_for :users
+  devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
+
   namespace :api, defaults: { format: :json  }, constraints: { subdomain: "api" }, path: "/" do
     api_version(:module => "V1", :path => {:value => "v1"}) do
       resources :users, only: [:show, :index, :create, :update, :destroy]
+      resources :sessions, only: [:create]
     end
   end
 end
