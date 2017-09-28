b6e41b65dd687a46889b27edcf8df47ba8c813a0
Otavio Henrique <otavio.henrique@hotmail.com>
2017-09-28 20:46:41 -0300

    Destroy dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :tasks, dependent: :destroy
+
   before_create :generate_authentication_token!
   validates_uniqueness_of :auth_token
 
