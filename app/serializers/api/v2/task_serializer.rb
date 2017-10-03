a74e8dfedbd8c168b6c538a2a31f65964915e034
Otavio Henrique <otavio.henrique@hotmail.com>
2017-10-03 20:05:56 -0300

    Task Serializer

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,3 +1,14 @@
 class Api::V2::TaskSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :description, :short_description, :done, :is_late, 
+              :deadline, :user_id, :created_at, :updated_at
+
+  def short_description
+    object.description[0..40]
+  end
+
+  def is_late
+    Time.current > object.deadline if object.deadline.present?
+  end
+  
+  belongs_to :user
 end
