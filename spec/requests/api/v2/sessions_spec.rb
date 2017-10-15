3ee4dcd324de123af19b3ea5acfa138de4c9c3de
Otavio Henrique <otavio.henrique@hotmail.com>
2017-10-15 18:23:41 -0200

    sessions controllers and sending more parameters for devise

--- spec/requests/api/v2/sessions_spec.rb
+++ spec/requests/api/v2/sessions_spec.rb
@@ -1,61 +1,66 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Sessions API', type: :request do 
+RSpec.describe "Sessions API", type: :request do 
   before { host! "api.task-manager.dev:80/v2" }
   
   let(:user) { create(:user) }
-  let(:headers) do 
+  let!(:auth_data) { user.create_new_auth_token }
+  let(:headers) do
     {
+      "Content-type" => Mime[:json].to_s,
       "Accept" => "application/json",
-      "Content-Type" => Mime[:json].to_s
+      "access-token" => auth_data["access-token"],
+      "uid" => auth_data["uid"],
+      "client" => auth_data["client"]
     }
   end
 
-  describe 'POST /sessions' do
+  describe "POST /auth/sign_in" do
     before do 
-      post '/sessions', params: { session: credentials }.to_json, headers: headers
+      post "/auth/sign_in", params: credentials.to_json, headers: headers
     end
 
-    context 'when the credentials are correct' do
-      let(:credentials) { { email: user.email, password: 'password' } }
+    context "when the credentials are correct" do
+      let(:credentials) { { email: user.email, password: "password" } }
 
-      it 'return 200 status' do
+      it "return 200 status" do
         expect(response).to have_http_status(200)
       end
 
-      it 'returns json data with current signed in user and token' do
-        user.reload
-        expect(json_body["data"]["attributes"]["auth-token"]).to eq(user.auth_token)
+      it "returns the authentication data in the headers" do
+        expect(response.headers).to have_key("access-token")
+        expect(response.headers).to have_key("uid")
+        expect(response.headers).to have_key("client")
       end
     end
 
-    context 'when the credentials are incorrect' do
-      let(:credentials) { { email: user.email, password: 'invalid_password' } }
+    context "when the credentials are incorrect" do
+      let(:credentials) { { email: user.email, password: "invalid_password" } }
 
-      it 'return 401 status' do
+      it "return 401 status" do
         expect(response).to have_http_status(401)
       end
 
-      it 'returns json data with errors' do
-        user.reload
+      it "returns json data with errors" do
         expect(json_body).to have_key("errors")
       end
     end
   end
 
-  describe 'DELETE /sessions/:id' do
+  describe "DELETE /auth/sign_out" do
     let(:auth_token) { user.auth_token }
 
     before do
-      delete "/sessions/#{auth_token}", params: {}, headers: headers
+      delete "/auth/sign_out", params: {}, headers: headers
     end
 
-    it 'returns 204 status' do
-      expect(response).to have_http_status(204)
+    it "returns 200 status" do
+      expect(response).to have_http_status(200)
     end
 
-    it 'changes user auth token' do
-      expect( User.find_by(auth_token: auth_token) ).to be_nil
+    it "changes user auth token" do
+      user.reload
+      expect(user).not_to be_valid_token(auth_data["access-token"], auth_data["client"])
     end
   end
 end
